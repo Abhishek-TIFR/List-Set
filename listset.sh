e3ac4face5b8b43aeb0b9aa1d6cc6f0e2a4d43ff
Abhishek Kr Singh <abhishek.uor@gmail.com>
2018-09-10 14:17:04 +0530

    run to compile listset files

---
+++ listset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+coqc GenReflect.v
+coqc SetSpecs.v
+coqc Sorting.v
+coqc MinMax.v
+coqc DecType.v
+coqc SetReflect.v
+coqc DecList.v
+coqc MoreDecList.v
+coqc OrdType.v
+coqc OrdList.v
+coqc OrdSet.v
+coqc SetMaps.v
