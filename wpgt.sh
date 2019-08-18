f870603a1781c44542f3707d6a265023da64574a
Abhishek Kr Singh <abhishek.uor@gmail.com>
2019-08-19 01:34:39 +0530

    Repeat and LovaszRep added

--- wpgt.sh
+++ wpgt.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-printf ‘UG
-coqc UG.v
 
-printf ‘MoreUG
-coqc MoreUG.v
+printf ‘Repeat
+coqc Repeat.v
 
-printf ‘GenIso
-coqc GenIso.v
-
-printf ‘PreLovasz
-coqc PreLovasz.v
+printf ‘LovaszRep
+coqc LovaszRep.v
 
 
 echo \
