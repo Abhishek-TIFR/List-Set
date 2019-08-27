a20f262c49f427f0cd7abe89ec809ea4701df23c
Abhishek Kr Singh <abhishek.uor@gmail.com>
2019-08-28 01:35:48 +0530

    one lemma remainng

--- WeakPerfect.v
+++ WeakPerfect.v
@@ -19,16 +19,174 @@ Section WPerfect.
   Definition meets (K I: list A):= exists a:A, In a K /\ In a I. 
 
   Lemma Cliq_meets_all_MaxI (G: @UG A):
-    Perfect G -> (exists K, Cliq_in G K /\ (forall I, Max_I_in G I -> meets K I)).
+    (nodes G)<> nil-> Perfect G -> (exists K, Cliq_in G K /\ (forall I, Max_I_in G I -> meets K I)).
   Proof. Admitted.
-  
 
+  Lemma cliq_meets_stable_once (G: @UG A)(K: list A)(I: list A)(x y: A):
+    Cliq G K -> Stable G I -> In x K -> In y K -> In x I -> In y I -> x = y.
+  Proof. { intros h1 h2 hxk hyk hxi hyi. unfold Cliq in h1;unfold Stable in h2.
+           specialize (h1 x y hxk hyk) as h1a.
+           destruct h1a as [h1a | h1b].
+           { auto. }
+           { absurd (edg G x y). switch;apply h2;auto. auto. } } Qed.
+  
+  
   Lemma i_num_cliq_cover (G: @UG A)(n: nat):
     Perfect G -> i_num G n -> (exists C, Cliq_cover C G /\ |C| = n).
-  Proof. Admitted.
+  Proof. { revert G. induction n using strong_induction.
+           intros G h1 h2.
+           destruct h2 as [I h2]. destruct h2 as [h2a h2].
+           assert (hN: nodes G = nil \/ nodes G <> nil). eauto.
+           destruct hN as [hNa | hNb].
+           {(*---- When the graph is empty Graph i.e nodes G = nil  ---------*)
+             exists nil. split.
+             { (*-- Cliq_cover nil G --*)
+               unfold Cliq_cover. split.
+               unfold Set_cover. rewrite hNa. simpl. auto.
+               simpl. tauto. }
+             { (*---  (| nil |) = n ----*)
+               simpl.
+               assert (hI: I = nil). 
+               { destruct h2a as [h2a h2b]. rewrite hNa in h2a.
+                 apply set_equal. apply h2b. constructor. split; auto. }
+               subst I. simpl in h2. auto. }   } 
 
-  
+           {(*----- When the graph is non-empty i.e nodes G <> nil ----------*)
+             specialize (Cliq_meets_all_MaxI hNb h1) as h1a. 
+             destruct h1a as [K h1a]. destruct h1a as [h1a h1b]. 
+             assert (hg: exists g, In g G). auto.
+             destruct hg as [g hg].
+             
+             assert (hn: n >= 1).
+             { set (I':= g::nil). 
+               assert (hI': Stable_in G I').
+               { unfold Stable_in. subst I'.
+                 split.
+                 { intros x hx. destruct hx as [hx |hx];simpl in hx. subst x;auto. tauto. }
+                 split.
+                 { constructor. }
+                 { unfold Stable. intros x y hx hy.
+                   assert (hxy: x = y).
+                   { destruct hx as [hx | hx];destruct hy as [hy | hy].
+                     subst x; auto. simpl in hy;tauto. all: simpl in hx;tauto. }
+                    subst x. auto. } }
+               assert (h3: |I'| <= |I|).
+               { eauto. }
+               subst I'. simpl in h3. rewrite h2 in h3.
+               auto. }
+             
+             assert (h1c: K [<=] G). auto.
+             assert (h1d: K = K [i] G). eauto.
+         
+             (* G_K represents the graph obtained by removing cliq K from graph G *)
+             set (G_K:= ind_at (G [\] K) G).
+             assert (hg_k: Ind_subgraph G_K G).
+             { subst G_K. auto. }
+
+             assert (hG_K: Perfect G_K).
+             { cut (Ind_subgraph G_K G). eauto. unfold G_K; auto. }
+
+             assert (h3: i_num G_K (n-1)). 
+             { specialize (h1b I h2a) as hKI.
+               destruct hKI as [k hKI].
+               set (I_k:= rmv k I).
+               exists I_k.
+               split.
+               { (*--  Max_I_in G_K I_k --*)
+                 assert (h3: Stable_in G I). auto. unfold Stable_in in h3.
+                 assert (h3a: Stable G I). apply h3. unfold Stable in h3a.
+                 assert (h9: I_k [<=] G_K ).
+                 { subst I_k; subst G_K. simpl.
+                   replace ((G [\] K) [i] G) with (G [\] K).
+                   Focus 2. cut (G [\] K [<=] G); auto.
+                   intros x hx.
+                   assert (hx1: In x I). eauto.
+                   assert (hxk: x <> k).
+                   { cut (NoDup I).  eauto. cut (IsOrd I). auto. apply h3. }
+                   cut (In x G). cut (~ In x K). auto.
+                   intro h4. absurd (x = k). auto.
+                   eapply cliq_meets_stable_once with (G:= G)(K:= K)(I:= I).
+                   all: auto. apply hKI. apply hKI. cut (I [<=] G ). auto.
+                   apply h3. }
+                 assert (h10:  (| I_k |) = n - 1).
+                 { subst I_k. cut (In k I).  rewrite <- h2. eauto.
+                   apply hKI. } 
+                 split.
+                 { (*-- I_k [<=] G_K --*)
+                   apply h9. }
+                 split.
+                 { (*--- IsOrd I_k---*)
+                   subst I_k. cut (IsOrd I). auto. apply h3. }
+                 split.
+                 { (*--- stable G_K I_k----*)
+                   apply /stableP. unfold Stable.
+                   intros x y hx hy. replace (edg G_K x y) with (edg G x y).
+                   apply h3a; unfold I_k in hx, hy;eauto.
+                   symmetry. cut (In x G_K). cut (In y G_K). all: auto. }
+                 { (*- (forall I' :list A, In I' (pw G_K)-> stable G_K I'-> (|I'|)<=(|I_k|)) -*)
+                   intros I' h11 h12. move /stableP in h12.
+                   assert (h13: Stable_in G I').
+                   { cut (Stable_in G_K I'). eauto. 
+                     unfold Stable_in. split. auto. split;auto.
+                     cut (IsOrd G_K). eauto. subst G_K;simpl. auto. }
+                   cut ( ~ (| I' |) > (| I_k |)). omega.
+                   intros h14.
+                   assert (h15: (|I'|) <= n).
+                   { rewrite <- h2. eauto. } 
+                   rewrite h10 in h14.
+                   assert (h16: |I'| = n). omega.
+                   assert (h17: Max_I_in G I').
+                   { apply Max_I_in_intro. auto.
+                     rewrite h16. rewrite <- h2. intros I0' h17. eauto. }
+                   specialize (h1b I' h17) as h18. destruct h18 as [k' h18].
+                   assert (h19: I' [<=] G_K).
+                   { cut (IsOrd G_K). eauto. subst G_K;simpl. auto. }
+                   absurd (In k' K). cut (In k' G_K).
+                   subst G_K. simpl.
+                   replace ((G [\] K) [i] G) with (G [\] K).
+                   Focus 2. cut (G [\] K [<=] G); auto.
+                   intros h20. eauto. cut (In k' I'). auto. all: apply h18. } }
+               
+               { (*--- (| I_k |) = n - 1 --*)
+                  subst I_k. cut (In k I).  rewrite <- h2. eauto.
+                  apply hKI.  } } 
+
+             assert (h4: n - 1 < n). omega.
+
+             specialize (H (n-1) h4 G_K hG_K h3) as h5.
+             destruct h5 as [C_K h5].
+
+             exists (K::C_K).
+             split.
+             { (*--- Cliq_cover (K :: C_K) G -----*)
+               unfold Cliq_cover.
+               split.
+               {(*---- Set_cover (K :: C_K) G ----*)
+                 unfold Set_cover.
+                 assert (h6: nodes G_K = G [\] K).
+                 { unfold G_K. simpl. symmetry.
+                   cut ((G [\] K) [<=] G). auto. auto. }
+                 replace (nodes G) with (K [u] (G [\] K)).
+                 Focus 2. symmetry;auto. simpl.
+                 replace ((G [\] K)) with (union_over C_K).
+                 Focus 2. symmetry. rewrite <- h6.   apply h5.
+                 auto. } 
+               
+               {(*---- (forall I0 : list A, In I0 (K :: C_K) -> Cliq G I0 /\ IsOrd I0) ---*)
+                 intros Kx h6. destruct h6 as [h6 | h6]. 
+                 { rewrite <- h6. split;auto. eauto. }
+                 { assert (h5c: Cliq_cover C_K G_K). apply h5.
+                   split. cut (Cliq_in G Kx). auto.
+                   cut (Cliq_in G_K Kx). all: eauto. }  } }
+             
+             { (*---- (| K :: C_K |) = n ---------*)
+               destruct h5 as [h5a h5].
+               simpl. rewrite h5. omega. }   } } Qed.
+           
+          
+ 
 End WPerfect.
+
 
 
 Section WPGT.
